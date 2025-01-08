266a7e3a40453a649555c93afd1369c1a94134cd
nathanielpierson <nathanielepierson@gmail.com>
2025-01-07 21:50:04 -0500

    edit store-item

--- store-item.rb
+++ store-item.rb
@@ -1,9 +1,14 @@
-# C. Use hashes with symbols to represent the following scenario:
-# C.1 - You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
-# C.2 - Represent 3 items using hashes. Each hash should have the same keys with different values.
-# C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
+class Fruits
+  def initialize (traits)
+    @color = traits[:color]
+    @price = traits[:price]
+    @location = traits[:location]
+    @best_fruit = traits[:best_fruit]
+  end
+  attr_reader :color, :price, :location, :bestfruit
+end
 
-banana = {color: "yellow", price: "1.99", location_of_origin: "Venezuela", best_fruit: false}
-raspberry = {color: "pink", price: "4.99", location_of_origin: "My fridge", best_fruit: true}
-orange = {"color" => "orange, duh", price => "2.54", location_of_orangin: "Africa", best_fruit: false}
-puts banana[:best_fruit]
+banana = Fruits.new({color: "yellow", price: 1.99, location: "Venezuela", best_fruit: false})
+dragonfruit = Fruits.new({color: "magenta", price: 3.69, location: "Dragon Roost Island", best_fruit: false})
+raspberry = Fruits.new({color: "pink", price: 0.41, location: "America", best_fruit: true})
+pp "Bananas are #{banana.color}, dragonfruit cost #{dragonfruit.price}, and raspberries are from #{raspberry.location}."
