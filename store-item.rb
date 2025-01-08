47846fc388ab1bc3efbef1a32b2e2fd939c90069
nathanielpierson <nathanielepierson@gmail.com>
2025-01-07 21:59:01 -0500

    edit store-item.rb

--- store-item.rb
+++ store-item.rb
@@ -5,10 +5,23 @@ class Fruits
     @location = traits[:location]
     @best_fruit = traits[:best_fruit]
   end
-  attr_reader :color, :price, :location, :bestfruit
+  attr_reader :color, :price, :location, :best_fruit
+  attr_writer :color, :price, :location, :best_fruit
+  def best
+    if @best_fruit == true
+      puts "this is the best fruit"
+    else
+      puts "it's trash"
+    end
+  end
 end
 
 banana = Fruits.new({color: "yellow", price: 1.99, location: "Venezuela", best_fruit: false})
 dragonfruit = Fruits.new({color: "magenta", price: 3.69, location: "Dragon Roost Island", best_fruit: false})
 raspberry = Fruits.new({color: "pink", price: 0.41, location: "America", best_fruit: true})
 pp "Bananas are #{banana.color}, dragonfruit cost #{dragonfruit.price}, and raspberries are from #{raspberry.location}."
+raspberry.location = "Greenland"
+pp "Bananas are #{banana.color}, dragonfruit cost #{dragonfruit.price}, and raspberries are from #{raspberry.location}."
+dragonfruit.best
+dragonfruit.best_fruit = true
+dragonfruit.best
